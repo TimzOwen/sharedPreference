985589bc20166438b55936e999a8b75838a21941
TimzOwen <ktimothy@kabarak.ac.ke>
2020-08-31 13:06:12 +0300

    soln to radio buttons and background color

--- app/src/main/java/com/owen/sharedpreference/MainActivity.java
+++ app/src/main/java/com/owen/sharedpreference/MainActivity.java
@@ -1,14 +1,46 @@
 package com.owen.sharedpreference;
 
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    //step 1
+    //get the elements and views used.
+    ConstraintLayout mConstraintLayout;
+    RadioGroup mRadioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //step 2 . find the views form XML
+        mConstraintLayout = findViewById(R.id.contraint_layout);
+        mRadioGroup = findViewById(R.id.rGroup_view);
+
+        //step 3 . Set oncheckedChange listeners
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                //Step 4 .Check for the radio button selected by the user
+                if (checkedId==R.id.rbtnBlue) {
+                    mConstraintLayout.setBackgroundColor(Color.BLUE);
+                }
+                else if (checkedId==R.id.rbtnYellow)
+                {
+                    mConstraintLayout.setBackgroundColor(Color.YELLOW);
+                }
+                else if (checkedId==R.id.rbtnRed)
+                {
+                    mConstraintLayout.setBackgroundColor(Color.MAGENTA);
+                }
+            }
+        });
+
     }
 }
