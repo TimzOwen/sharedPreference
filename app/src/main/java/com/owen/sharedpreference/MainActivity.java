d46ffe023175c1f2bc05f1a295893351d91dce20
TimzOwen <ktimothy@kabarak.ac.ke>
2020-08-31 13:36:16 +0300

    sharedpreference update soln

--- app/src/main/java/com/owen/sharedpreference/MainActivity.java
+++ app/src/main/java/com/owen/sharedpreference/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.RadioGroup;
@@ -19,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //step 5 . instantiate the sharedpreference
+        SharedPreferences sharedPreferences = getSharedPreferences("background", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //step 8 . check if the preference contains a color ID
+        if (sharedPreferences.contains("colorID"))
+           mConstraintLayout.setBackgroundColor(sharedPreferences.getInt("colorID",0));
+
         //step 2 . find the views form XML
         mConstraintLayout = findViewById(R.id.contraint_layout);
         mRadioGroup = findViewById(R.id.rGroup_view);
@@ -27,18 +36,24 @@ public class MainActivity extends AppCompatActivity {
         mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
+                //step 6 . Add variable to hold the color codes.
+                int colorCodeID = 0;
                 //Step 4 .Check for the radio button selected by the user
-                if (checkedId==R.id.rbtnBlue) {
-                    mConstraintLayout.setBackgroundColor(Color.BLUE);
+                switch (checkedId) {
+                    case R.id.rbtnBlue:
+                        colorCodeID = Color.BLUE;
+                        break;
+                    case R.id.rbtnYellow:
+                        colorCodeID = Color.YELLOW;
+                        break;
+                    case R.id.rbtnRed:
+                        colorCodeID = Color.MAGENTA;
+                        break;
                 }
-                else if (checkedId==R.id.rbtnYellow)
-                {
-                    mConstraintLayout.setBackgroundColor(Color.YELLOW);
-                }
-                else if (checkedId==R.id.rbtnRed)
-                {
-                    mConstraintLayout.setBackgroundColor(Color.MAGENTA);
-                }
+                //step 7 .setbackground color to the colorCodeId
+                mConstraintLayout.setBackgroundColor(colorCodeID);
+                editor.putInt("colorID",colorCodeID);
+                editor.apply();
             }
         });
 
